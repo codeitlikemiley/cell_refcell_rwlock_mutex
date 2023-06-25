8151c5f609e462a5f0940f1198dbd8e491919f9b
Uriah <codeitlikemiley@gmail.com>
2023-06-25 21:26:59 +0800

    added lifetime parameter on Node

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-
 #[derive(Debug)]
-struct Node {
+// add unused attribute to suppress warnings, and derived debug
+#[allow(dead_code)]
+// we add a lifetime parameter to the struct
+struct Node<'a> {
     value: i32,
-    adjacent: Vec<&Node>
+    adjacent: Vec<&'a Node<'a>>,
 }
-
-
 
 
 fn main() {
     let a =  Node { value: 1, adjacent: vec![] };
     let b = Node { value: 2, adjacent: vec![&a]};
+    let c  = Node { value: 3, adjacent: vec![&a, &b]};
 
-    dbg!(a);
+    dbg!(&a);
+    dbg!(&b);
+    dbg!(&c);
 }
