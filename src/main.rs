631c503416aa312fa473195afd63a6fb38c930af
Uriah <codeitlikemiley@gmail.com>
2023-06-25 21:50:15 +0800

    added comment on when we should use Cell

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use std::cell::Cell;
 #[derive(Debug)]
 #[allow(dead_code)]
 struct Node<'a> {
+    // we use Cell to allow mutation of the value field
+    // if we have a piece of data that is copyable
+    // and we dont mind copying it , which not gonna be a performance hit
+    // just like in this case with i32 , then Cell is a good choice
+    // If we have something that is not copyable like a [String] , then we can use RefCell
     value: Cell<i32>,
     adjacent: Vec<&'a Node<'a>>,
 }
@@ -12,8 +17,14 @@ fn add_one(node: &Node) {
     let current_value = node.value.get();
     node.value.set(current_value + 1);
 
-    for adj in &node.adjacent {
-        add_one(adj);
+    // for adj in &node.adjacent {
+        // add_one(&adj);
+    // }
+
+    // we use iter() to grab shared references to the adjacent nodes
+    // we dont wanna take ownership here
+    for adj in node.adjacent.iter() {
+        add_one(&adj);
     }
 }
 
@@ -27,19 +38,14 @@ fn main() {
         adjacent: vec![&a],
     };
     let c = Node {
-        value: Cell::new(3),
-        adjacent: vec![&a, &b],
+        value: Cell::new(4),
+        adjacent: vec![&a],
     };
 
-    dbg!(&a);
-    dbg!(&b);
-    dbg!(&c);
-
-    add_one(&c);
+    add_one(&b);
 
     println!("After adding one to all nodes:");
 
     dbg!(&a);
     dbg!(&b);
-    dbg!(&c);
 }
