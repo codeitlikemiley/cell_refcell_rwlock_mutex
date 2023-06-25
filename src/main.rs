21185569ac226854b14722e8f3649d7f9d54d68c
Uriah <codeitlikemiley@gmail.com>
2023-06-25 21:30:10 +0800

    error on borrowed value , we are trying to mutate

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,14 @@ struct Node<'a> {
     adjacent: Vec<&'a Node<'a>>,
 }
 
+// this causes error , since we are trying to mutate a borrowed value
+fn add_one(node: Node) {
+    node.val = node.value + 1;
+    for adj in node.adjacent {
+        add_one(&adj);
+    }
+}
+
 
 fn main() {
     let a =  Node { value: 1, adjacent: vec![] };
