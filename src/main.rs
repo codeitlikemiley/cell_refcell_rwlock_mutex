3e28c82e2f184c2c797949466ed73e7a3139f5ff
Uriah <codeitlikemiley@gmail.com>
2023-06-25 21:33:00 +0800

    added comment of rust interior mutability solutions

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,11 @@ struct Node<'a> {
 }
 
 // this causes error , since we are trying to mutate a borrowed value
+// to fix this issue we can use rust 4 types of interior mutability
+// 1. Cell
+// 2. RefCell
+// 3. Mutex
+// 4. RwLock
 fn add_one(node: &Node) {
     node.value = node.value + 1;
     for adj in node.adjacent {
