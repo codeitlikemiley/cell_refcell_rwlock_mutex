d91bceb5c8cfb4ed5b31546582405ceba1e92922
Uriah <codeitlikemiley@gmail.com>
2023-06-25 21:39:39 +0800

    using cell , on Node

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,43 @@
+use std::cell::Cell;
+
 #[derive(Debug)]
-// add unused attribute to suppress warnings, and derived debug
 #[allow(dead_code)]
-// we add a lifetime parameter to the struct
 struct Node<'a> {
-    value: i32,
+    value: Cell<i32>,
     adjacent: Vec<&'a Node<'a>>,
 }
 
-// this causes error , since we are trying to mutate a borrowed value
-// to fix this issue we can use rust 4 types of interior mutability
-// 1. Cell
-// 2. RefCell
-// 3. Mutex
-// 4. RwLock
 fn add_one(node: &Node) {
-    node.value = node.value + 1;
-    for adj in node.adjacent {
-        add_one(&adj);
+    // we have setter and getter methods added by Cell
+    let current_value = node.value.get();
+    node.value.set(current_value + 1);
+
+    for adj in &node.adjacent {
+        add_one(adj);
     }
 }
 
-
 fn main() {
-    let a =  Node { value: 1, adjacent: vec![] };
-    let b = Node { value: 2, adjacent: vec![&a]};
-    let c  = Node { value: 3, adjacent: vec![&a, &b]};
+    let a = Node {
+        value: Cell::new(1),
+        adjacent: vec![],
+    };
+    let b = Node {
+        value: Cell::new(2),
+        adjacent: vec![&a],
+    };
+    let c = Node {
+        value: Cell::new(3),
+        adjacent: vec![&a, &b],
+    };
+
+    dbg!(&a);
+    dbg!(&b);
+    dbg!(&c);
+
+    add_one(&c);
+
+    println!("After adding one to all nodes:");
 
     dbg!(&a);
     dbg!(&b);
